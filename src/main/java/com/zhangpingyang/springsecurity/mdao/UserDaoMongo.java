af72b1c03cb04a90c6d3fd99a9eaf0cb65ab0a7f
pingyang <285442936@qq.com>
2019-09-10 19:03:39 +0800

    123

---
+++ src/main/java/com/zhangpingyang/springsecurity/mdao/UserDaoMongo.java
@@ -0,0 +1,10 @@
+package com.zhangpingyang.springsecurity.mdao;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @Author: Zhang Pingyang
+ * @Date: 2019/9/10 19:01
+ */
+public interface UserDaoMongo extends CrudRepository {
+}
